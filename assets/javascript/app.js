23d9a96c5962ba16ea71e935ee6343277290f427
Emily McKenna <emilymckenna08@gmail.com>
2018-05-16 18:55:59 -0400

    added questions obj

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+//set variables
+var numCorrect;
+var userGuess;
+var answers;
+var correctAnswer;
+
+
+//creates object to hold questions and answers
+
+var questions = [{
+    question:"Who goes on Ross' honeymoon after Ross chases after Emily?",
+    answers:["Phoebe","Ugly Naked Guy","Janice","Rachel"],
+    correctAnswer: 3,
+},{
+    question:"Who is Tag?",
+    answers:["Monica's brother","Rachel's assistant","Joey's neighbor","Gunther's alter ego"],
+    correctAnswer: 1,
+},{
+    question:"What is Monica's profession?",
+    answers:["barista","cardiologist","chef","model"],
+    correctAnswer:2,
+},{
+    question:"What is the statue that Joey buys for his apartment?",
+    answers:["a fishnet-clad woman's leg","Julius Caesar","a dog","a bust of Chandler"],
+    correctAnswer:2,
+
+}];
+//arrays for each set of possible answers for each question
+
+//sets correct answers for each question
+
+//listen for user click and record answer
+
+//compare user guess to correct guess for each question using a for loop
+
+//reset function whenever user moves on to next question
+
+//tallies score whenever user hits submit buttom
+
+//sets timer that runs reset function whenever time is up
+
