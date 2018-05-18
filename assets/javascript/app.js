00c3b2b2b2dc3812744e063fbf69a1ce18f3ea3d
Emily McKenna <emilymckenna08@gmail.com>
2018-05-17 23:44:33 -0400

    runs through questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,6 +27,8 @@ var questions = [{
     correctAnswer:2,
 
 }];
+
+
 //starts game when button is clicked
 $("#start").on("click", startGame);
 
@@ -35,45 +37,68 @@ $("#start").on("click", startGame);
 //function to compare user guess to correct guess for each question using a for loop
 
 
-//if user answer is the same as the answers[correctAnswer], then user wins
+
+function grabGuess() {
+    userGuess = $("input:checked").val();
+    console.log(userGuess)
+    checkAnswer();
+}
+     
+ 
 
 
-
-//listen for user to click submit button and then run userSubmit
 
 //move on to next question whenever user picks an answer; increase currentQuestion?
 
-$("#next").on("click", function(){
+
+
+$(document).on("click", "#next", function(){
 
     currentQuestion++;
-    console.log(currentQuestion)
+    grabGuess();
     nextQuestion();
 
 });
 
+// //listen for user to click an answer and then run checkAnswer
 
 //start game
 function startGame() {
+    numCorrect=0;
+    numIncorrect=0;
     $("#start").remove();
-    $("#userQuestion").html("<p>" + questions[currentQuestion].question + "</p>");
-    $("#first").html("<input type='radio'>" + questions[currentQuestion].answers[0] + "</input>");
-    $("#second").html("<input type='radio'>" + questions[currentQuestion].answers[1] + "</input>");
-    $("#third").html("<input type='radio'>" + questions[currentQuestion].answers[2] + "</input>");
-    $("#fourth").html("<input type='radio'>" + questions[currentQuestion].answers[3] + "</input>");
     $("#possibleAnswers").append("<button id='next'>Continue</button>");
+    nextQuestion();
 }
 //push new question to HTML
 function nextQuestion() {
-  
+    
     $("#userQuestion").html("<p>" + questions[currentQuestion].question + "</p>");
-    $("#first").html("<input type='radio'>" + questions[currentQuestion].answers[0] + "</input>");
-    $("#second").html("<input type='radio'>" + questions[currentQuestion].answers[1] + "</input>");
-    $("#third").html("<input type='radio'>" + questions[currentQuestion].answers[2] + "</input>");
-    $("#fourth").html("<input type='radio'>" + questions[currentQuestion].answers[3] + "</input>");
-    $("#possibleAnswers").append("<button id='next'>Continue</button>");
+    $("#first").html("<input type='radio' id='radio0' value='0'>" + questions[currentQuestion].answers[0] + "</input>");
+    $("#second").html("<input type='radio' id='radio1'value='1'>" + questions[currentQuestion].answers[1] + "</input>");
+    $("#third").html("<input type='radio' id='radio2'value='2'>"+ questions[currentQuestion].answers[2] + "</input>");
+    $("#fourth").html("<input type='radio' id='radio3'value='3'>" + questions[currentQuestion].answers[3] + "</input>");
+    //$("#possibleAnswers").append("<button id='next'>Continue</button>");
 
 
 }
+
+//if user answer is the same as the answers[correctAnswer], then user wins
+function checkAnswer(){
+    if (parseInt(userGuess) === questions[currentQuestion].correctAnswer) {
+        numCorrect++;
+        console.log("number right: "+numCorrect);
+    }
+
+    else {
+        numIncorrect++;
+        console.log("number wrong:" + numIncorrect);
+    }
+
+    
+}
+
+
 
 //tallies score whenever user has answered all the questions
 
